2db970c7af632c14850b7645d8234654af90ff02
gemy26 <ahmed.gamal.5511@icloud.com>
2026-04-23 23:13:52 +0200

    [FIX] fixed the iterator NEXT it includes deleted entries now and include tombstone to the entry

--- internal/memtable/iterator.go
+++ internal/memtable/iterator.go
@@ -2,43 +2,37 @@ package memTable
 
 type Iterator struct {
 	curr *Node
-	end  int
+	end  int64
 }
 
 func (i *Iterator) Next() bool {
 	if i.curr == nil {
 		return false
 	}
-
 	i.curr = i.curr.next[0]
-	for i.curr != nil && i.curr.val.tombstone == true { //skip all deleted nodes
-		i.curr = i.curr.next[0]
-	}
-	if i.curr == nil {
-		return false
-	}
-	if i.curr != nil && i.curr.key > i.end {
+	if i.curr == nil || i.curr.key > i.end {
 		i.curr = nil
 		return false
 	}
 	return true
 }
 
-func (i *Iterator) Key() int {
+func (i *Iterator) Key() int64 {
 	return i.curr.key
 }
 
-func (i *Iterator) Value() int {
+func (i *Iterator) Value() int64 {
 	return i.curr.val.data
 }
 
 func (i *Iterator) Valid() bool {
-	return i.curr != nil && !i.curr.val.tombstone
+	return i.curr != nil
 }
 
 func (it *Iterator) Entry() Entry {
 	return Entry{
-		Key: it.curr.key,
-		Val: it.curr.val.data,
+		Key:        it.curr.key,
+		Val:        it.curr.val.data,
+		Tombstoned: it.curr.val.tombstone,
 	}
 }
