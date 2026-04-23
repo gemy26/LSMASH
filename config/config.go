77564dd9aaeb01e1dce10878d4eae5b163c0064b
gemy26 <ahmed.gamal.5511@icloud.com>
2026-04-23 23:12:25 +0200

    [FEAT] added a new configs: sstable size and sstable files size limits, also added working directory to store the sstable files

--- config/config.go
+++ config/config.go
@@ -1,15 +1,32 @@
 package config
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type Config struct {
-	MemTableSizeLimit   int64
-	SkipListMaxLevels   int
-	SkipListprobability float64
+	MemTableSizeLimit     int64
+	SkipListMaxLevels     int
+	SkipListprobability   float64
+	SstableFileSizeLimit  int64
+	SstablelevelSizeLimit int64 //level 0
+	WorkingDir            string
 }
 
 func DefaultConfig() Config {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	dataDir := filepath.Join(wd, "data")
+	_ = os.MkdirAll(dataDir, 0755)
 	return Config{
-		MemTableSizeLimit:   32000, //16 * 200
-		SkipListMaxLevels:   5,
-		SkipListprobability: 0.6,
+		MemTableSizeLimit:     85,
+		SkipListMaxLevels:     5,
+		SkipListprobability:   0.1,
+		SstableFileSizeLimit:  116,
+		SstablelevelSizeLimit: 348,
+		WorkingDir:            dataDir,
 	}
 }
