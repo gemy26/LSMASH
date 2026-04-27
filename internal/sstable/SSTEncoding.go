d7ea37035ec79d533fc07c71f69a30bccd1c1a1d
gemy26 <ahmed.gamal.5511@icloud.com>
2026-04-27 03:45:15 +0300

    [FIX] fixed readEntry and bound the loop by EntryCount to prevent infinite loop

--- internal/sstable/SSTEncoding.go
+++ internal/sstable/SSTEncoding.go
@@ -27,7 +27,10 @@ func (s *SSTable) writeHeader() error {
 	return nil
 }
 func (s *SSTable) writeEntry(entry []memTable.Entry) error {
-	s.file.Seek(headerSize, 0)
+	_, err := s.file.Seek(headerSize, 0)
+	if err != nil {
+		return err
+	}
 	for _, entryItem := range entry {
 		if err := binary.Write(s.file, binary.LittleEndian, entryItem.Key); err != nil {
 			return err
@@ -36,13 +39,17 @@ func (s *SSTable) writeEntry(entry []memTable.Entry) error {
 			return err
 		}
 		if err := binary.Write(s.file, binary.LittleEndian, entryItem.Tombstoned); err != nil {
+
 			return err
 		}
 	}
 	return nil
 }
 func (s *SSTable) writeBloom() error {
-	s.file.Seek(int64(s.header.BloomOffset), 0)
+	_, err := s.file.Seek(int64(s.header.BloomOffset), 0)
+	if err != nil {
+		return err
+	}
 	if err := binary.Write(s.file, binary.LittleEndian, s.bloom.bitset); err != nil {
 		return err
 	}
@@ -55,7 +62,6 @@ func (s *SSTable) readHeader() error {
 	return nil
 }
 func (s *SSTable) readBloom() error {
-	s.file.Seek(int64(s.header.BloomOffset), 0)
 	bitset := make([]byte, s.header.BloomSize)
 	if err := binary.Read(s.file, binary.LittleEndian, &bitset); err != nil {
 		return err
@@ -68,11 +74,24 @@ func (s *SSTable) readBloom() error {
 	}
 	return nil
 }
-func (s *SSTable) readEntry() []memTable.Entry {
-	data := make([]memTable.Entry, s.header.EntryCount)
-	s.file.Seek(headerSize, 0)
-	if err := binary.Read(s.file, binary.LittleEndian, &data); err != nil {
-		return nil
+
+func (s *SSTable) readEntry() ([]memTable.Entry, error) {
+	var data []memTable.Entry
+	if _, err := s.file.Seek(headerSize, 0); err != nil {
+		return nil, err
 	}
-	return data
+	for i := 0; i < int(s.header.EntryCount); i++ {
+		var entry memTable.Entry
+		if err := binary.Read(s.file, binary.LittleEndian, &entry.Key); err != nil {
+			return nil, err
+		}
+		if err := binary.Read(s.file, binary.LittleEndian, &entry.Val); err != nil {
+			return nil, err
+		}
+		if err := binary.Read(s.file, binary.LittleEndian, &entry.Tombstoned); err != nil {
+			return nil, err
+		}
+		data = append(data, entry)
+	}
+	return data, nil
 }
